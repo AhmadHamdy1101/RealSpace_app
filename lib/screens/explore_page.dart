9ca1e55da726b4df9c6cac09bcda1c480c298856
AhmadHamdy1101 <7amohamdy@gmail.com>
2025-03-12 22:34:01 +0200

    add expoloer page

--- lib/screens/explore_page.dart
+++ lib/screens/explore_page.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter/material.dart';
 
+import '../Widgets/PropertyListView.dart';
+import '../Widgets/searchBarWedget.dart';
+
 
 class ExplorePage extends StatelessWidget {
   const ExplorePage({super.key});
@@ -9,11 +12,47 @@ class ExplorePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-    
-      body: Text("heeeeelp")
+    return CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(child: Column( children: [
+            SizedBox(height: 10,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+              child: SearchBar_wedget(),
+            ),
+            SizedBox(height: 15,),
 
+            SizedBox(height: 15,),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Column(children: [
+                SizedBox(height: 15,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Top Property",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                    TextButton(onPressed: () {} , child: Text("See More"),)
+                  ],
+                ),
+                PropertyListView(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+
+                    Text("New Property",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                    TextButton(onPressed: () {} , child: Text("See More"),)
+                  ],
+                ),
+                SizedBox(height: 15,),
+                PropertyListView(),
+
+
+              ],),
+            ),
+
+
+          ],),
+          )]
     );
   }
 }
